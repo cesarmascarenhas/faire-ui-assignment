259171422419e8503b6eb4de2687146edb76a434
Cesar Mascarenhas <cesarmascarenhas@gmail.com>
2019-03-26 12:14:29 -0300

    Remove Link (Product Component no implemented)

--- front-end/src/Components/Product.js
+++ front-end/src/Components/Product.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import returnable_badge from '../Assets/returnable.svg';
 import custom_badge from '../Assets/custom.svg';
 
-import {Link} from 'react-router-dom';
-
 
 export default class Product extends Component {
   
@@ -22,28 +20,27 @@ export default class Product extends Component {
       images,
       returnable,
       custom,
-      categories,
-      token
+      categories
     } = this.props.product;
     const whole = wholesale_price_cents/100;
     return (
       <div className="brand-props">        
         <div className="brand-img product">
-            <Link to={`/brand/${token}`}>
+            
                 <img src={images[0].url} alt=""/>
-            </Link>
+            
             <div className="brand-badges">
               <ul>
                 {returnable ?  <li><img src={returnable_badge} alt=""/><span>Returnable</span></li> : ''}
                 {custom     ?  <li><img src={custom_badge} alt=""/><span>Custom</span></li> : ''}
-                {categories.length > 0 ? <li>{categories.slice(0,4).map(category=><Link  key={category} to={`/category/${category}`}><span>{category}</span></Link>)}
+                {categories.length > 0 ? <li>{categories.slice(0,4).map(category=><span>{category}</span>)}
                   { categories.length > 4 ? <span style={{fontSize:9,fontWeight:'bold'}}>And {categories.length - 4} more</span> : '' } </li> : ''}
               </ul>
             </div>
             <button>Mine!</button> 
         </div>
         <div>
-            <Link to={`/brand/${token}`}><p className="brand-name">{name}</p></Link>
+            <p className="brand-name">{name}</p>
             <p className="brand-minimum">{
               whole === 0 ? '' : `$${whole} Whole Sale` 
             }</p>
